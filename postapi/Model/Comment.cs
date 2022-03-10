0a3e957ff3b67f6a16331e4dae7538c6ab3df680
Anudeep <thotanudeep10@gmail.com>
2022-03-10 16:16:18 +0530

    Updated Review comments

---
+++ postapi/Model/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace postapi.Model
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentText { get; set; }
+
+        public int PostID { get; set; }
+
+        public DateTime CommentsCreatedDate { get; set; }
+
+        public DateTime CommentsUpdatedDate { get; set; }
+    }
+
+}
---
+++ postapi/Model/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace postapi.Model
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public string PostTittle { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public string DescriptionOfPost { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? LikesCount { get; set; }
+        public int? HeartCount { get; set; }
+
+    }
+}
